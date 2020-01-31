c96b30affbb2e09775ac1e78eef65f35c04e97a3
SriSaiNikhilKantipudi <54340600+SriSaiNikhilKantipudi@users.noreply.github.com>
2020-01-31 13:23:48 -0600

    Add files via upload

--- list.py
+++ list.py
@@ -1,8 +1,10 @@
-n = int(input("Enter n value:"))
-list = []       #initialized a list
-i = 0
-for i in range(n):
-    x = int(input("Enter weight of student in lbs:"))
-    list.insert(i, x)               #insert every student weight in lbs into list created
-    list[i] = list[i] *0.45         #converted weight into kgs
-print("Weight of students in kgs:", list)
+stu =int (input("Please enter the number of students: "))
+weightsLBs = []
+weightsKGs = []
+for n in range(stu):
+        w_lb = float(input("Enter the weight of student in lbs>>"))
+        weightsLBs.append(w_lb)
+
+weightsKGs = [weightsLBs[x]*0.453592 for x in range(stu)]
+
+print(weightsKGs)
